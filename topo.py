c29a5da276cf3af3b8c6b5deb95310f6a3f62a72
Govind Mittal <f2014530@pilani.bits-pilani.ac.in>
2018-05-18 17:01:35 +0530

    added comments

--- topo.py
+++ topo.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
 
+# argument list in order of their use in the script-- 
+#   1. the simulation time
+#   2. the link-bandwidth of the complete network 
+#   3. the adversary bombarding power
+
 from mininet.net import Mininet
 from mininet.node import Controller, RemoteController, OVSController
 from mininet.node import CPULimitedHost, Host, Node
@@ -15,8 +20,13 @@ import time
 import sys
 
 bw = float(sys.argv[2])
-def myNetwork():
 
+# definition of the network
+# the skeleton script was created after exporting the L2 script from the miniedit.py
+# to use miniedit run --> $ sudo ~/mininet/examples/mininet.py
+# then draw the topology in it. Example intel.mn present in the repo
+# then export L2 script using the menu bar.
+def myNetwork():
     net = Mininet( topo=None,
                    build=False,
                    ipBase='10.0.0.0/8')
@@ -42,13 +52,12 @@ def myNetwork():
     h3 = net.addHost('h3', cls=Host, ip='10.0.0.3', mac='00:00:00:00:00:03', defaultRoute=None)
     h4 = net.addHost('h4', cls=Host, ip='10.0.0.4', mac='00:00:00:00:00:04', defaultRoute=None)
     h5 = net.addHost('h5', cls=Host, ip='10.0.0.5', mac='00:00:00:00:00:05', defaultRoute=None)
+    # fixed the mac and ip of the adversary and dns for easy debugging
     adversary = net.addHost('adversary', cls=Host, ip='10.0.0.99', mac='00:00:00:00:00:99', defaultRoute=None)
     dns = net.addHost('dns', cls=Host, ip='10.0.0.100', mac='00:00:00:00:01:00', defaultRoute=None)
-    # trap = net.addHost('trap', cls=Host, ip='10.0.0.10', mac='00:00:00:00:00:10', defaultRoute=None)
-
+    
     info( '*** Add links\n')
-    # pdb.set_trace()
-
+    # adding links with bandwidth = bw
     net.addLink(h1, s1, cls=TCLink, **{'bw':bw})
     net.addLink(s2, h2, cls=TCLink, **{'bw':bw})
     net.addLink(adversary, s6, cls=TCLink, **{'bw':bw})
@@ -56,7 +65,6 @@ def myNetwork():
     net.addLink(h5, s5, cls=TCLink, **{'bw':bw})
     net.addLink(dns, s7, cls=TCLink, **{'bw':bw})
     net.addLink(s3, h3, cls=TCLink, **{'bw':bw})
-    # net.addLink(s7, trap, cls=TCLink, **{'bw':bw0})
     
     net.addLink(s1, s2, cls=TCLink, **{'bw':bw})
     net.addLink(s2, s3, cls=TCLink, **{'bw':bw})
@@ -82,12 +90,16 @@ def myNetwork():
 
     info( '*** Post configure switches and hosts\n')
     time_to_run = int(sys.argv[1], 10)
+    
+    # this statement sends the command to the calling node, and does not wait for reply.
     dns.sendCmd('python dns_raw.py > dns_'
             +str(bw)+'_'+ str(time_to_run))
     adversary.sendCmd('python adversary.py ' + sys.argv[3])
+    
     client = [h1, h2, h3, h4, h5] 
     i = 1
     for c in client:
+        # the command below sends the client number to the corresponding client script, used in client_raw.py
         c.sendCmd('python client_raw.py '+str(i))
         i += 1
     time.sleep(time_to_run)
